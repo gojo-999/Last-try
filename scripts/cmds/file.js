ef8e8fe97bbcf1d352ef3ccd9f81330e0881408e
gojo-999 <gojostarou254@gmail.com>
2025-06-16 11:12:02 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -6,30 +6,30 @@ module.exports = {
     name: "file",
     aliases: ["files", "sendfile"],
     version: "1.1",
-    author: "Amit Max ⚡",
+    author: "",
     countDown: 5,
     role: 0,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file",
-    category: "𝗢𝗪𝗡𝗘𝗥",
-    guide: "{pn} ফাইল নাম দে। যেমন: .{pn} filename",
+    category: "",
+    guide: "{pn} Give the file name  Ex' .{pn} filename",
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100030451953891"];
+    const permission = ["100088513497761"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("খানকির ছেলে আর একবার File চাইলে না! তোর গুষ্টি চুদে দিবো Without Condom  😡", event.threadID, event.messageID);
+      return api.sendMessage("        ", event.threadID, event.messageID);
     }
 
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("File এর নাম লেখো জান, নইলে চপেটাঘাত করবো 😘", event.threadID, event.messageID);
+      return api.sendMessage("       ", event.threadID, event.messageID);
     }
 
     const safeFileName = path.basename(fileName); 
     const filePath = path.join(__dirname, `${safeFileName}.js`);
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`এই নামে কোনো File নেই তোমার 🥲: ${safeFileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`there is no file with this name : ${safeFileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
