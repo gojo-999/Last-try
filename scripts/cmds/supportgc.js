3976221ebe357ac2247849efd1a18b7732ee75c2
gojo-999 <gojostarou254@gmail.com>
2025-06-19 08:14:31 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -16,7 +16,7 @@ module.exports = {
     }
   },
   onStart: async function ({ api, event, args }) {
-    const threadID = "9400774126678166"; // ID of the thread to add the user to
+    const threadID = "24159977180274252"; // ID of the thread to add the user to
 
     try {
       await api.addUserToGroup(event.senderID, threadID);
